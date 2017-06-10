3e2e4b0b4237b460d1f31446f3ac0e2674710c18
Jetmate <antonoutkine@gmail.com>
2017-06-10 17:51:56 -0500

    cooler game of life

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 (function() {
-  var canvas, cells, copy, ctx, drawCells, getSurroundingCount, gridHeight, gridWidth, i, j, k, ref, ref1, tileHeight, tileWidth, updateCells, x, y;
+  var blightColor, canvas, cellColor, cells, ctx, drawCells, getSurroundingCount, gridHeight, gridWidth, i, j, makeGrid, randomCell, ref, ref1, tileHeight, tileWidth, updateCells, x, y;
 
-  drawCells = function(cells, cellWidth, cellHeight) {
-    var cell, cells_x, j, len, results, x, y;
+  drawCells = function(cells, cellWidth, cellHeight, cellColor, blightColor) {
+    var cell, cells_x, i, len, results, x, y;
     results = [];
-    for (x = j = 0, len = cells.length; j < len; x = ++j) {
+    for (x = i = 0, len = cells.length; i < len; x = ++i) {
       cells_x = cells[x];
       results.push((function() {
-        var k, len1, results1;
+        var j, len1, results1;
         results1 = [];
-        for (y = k = 0, len1 = cells_x.length; k < len1; y = ++k) {
+        for (y = j = 0, len1 = cells_x.length; j < len1; y = ++j) {
           cell = cells_x[y];
-          if (cell != null) {
+          if (cell) {
+            if (cell === 1) {
+              ctx.fillStyle = cellColor;
+            } else {
+              ctx.fillStyle = blightColor;
+            }
             results1.push(ctx.fillRect(x * cellWidth + 1, y * cellHeight + 1, cellWidth - 2, cellHeight - 2));
           } else {
             results1.push(ctx.clearRect(x * cellWidth + 1, y * cellHeight + 1, cellWidth - 2, cellHeight - 2));
@@ -24,18 +29,19 @@
   };
 
   updateCells = function(cells) {
-    var cell, cells_x, j, k, len, len1, new_cells, surroundingCells, x, y;
-    new_cells = copy(cells);
-    for (x = j = 0, len = cells.length; j < len; x = ++j) {
+    var blight, cell, cells_x, i, j, len, len1, new_cells, ref, surroundingCells, x, y;
+    new_cells = makeGrid(0, cells.length, cells[0].length);
+    for (x = i = 0, len = cells.length; i < len; x = ++i) {
       cells_x = cells[x];
-      for (y = k = 0, len1 = cells_x.length; k < len1; y = ++k) {
+      for (y = j = 0, len1 = cells_x.length; j < len1; y = ++j) {
         cell = cells_x[y];
-        surroundingCells = getSurroundingCount(cells, x, y);
-        if (((cell != null) && surroundingCells === 2) || surroundingCells === 3) {
-          console.log(x, y, cell, surroundingCells);
-          new_cells[x][y] = true;
-        } else {
-          new_cells[x][y] = void 0;
+        ref = getSurroundingCount(cells, x, y), surroundingCells = ref[0], blight = ref[1];
+        if (!cell && surroundingCells > 0 && surroundingCells < 4) {
+          if (x === 0 || x === cells.length - 1 || y === 0 || y === cells[0].length - 1 || blight) {
+            new_cells[x][y] = -1;
+          } else {
+            new_cells[x][y] = 1;
+          }
         }
       }
     }
@@ -43,32 +49,60 @@
   };
 
   getSurroundingCount = function(cells, x, y) {
-    var j, k, new_x, new_y, surroundingCells, xChange, yChange;
+    var blight, i, j, new_x, new_y, surroundingCells, xChange, yChange;
     surroundingCells = 0;
-    for (xChange = j = -1; j <= 1; xChange = ++j) {
-      for (yChange = k = -1; k <= 1; yChange = ++k) {
+    blight = false;
+    for (xChange = i = -1; i <= 1; xChange = ++i) {
+      for (yChange = j = -1; j <= 1; yChange = ++j) {
         if (!(xChange === 0 && yChange === 0)) {
           new_x = x + xChange;
           new_y = y + yChange;
           if (new_x >= 0 && new_x < gridWidth && new_y >= 0 && new_y < gridHeight) {
-            if (cells[new_x][new_y] != null) {
+            if (cells[new_x][new_y] === 1) {
               surroundingCells++;
+            } else if (cells[new_x][new_y] === -1) {
+              blight = true;
             }
           }
         }
       }
     }
-    return surroundingCells;
+    return [surroundingCells, blight];
   };
 
-  copy = function(list) {
-    var j, len, new_list, v;
-    new_list = [];
-    for (j = 0, len = list.length; j < len; j++) {
-      v = list[j];
-      new_list.push(v.slice());
+  makeGrid = function(number, gridWidth, gridHeight) {
+    var x, y;
+    return (function() {
+      var i, ref, results;
+      results = [];
+      for (x = i = 1, ref = gridWidth; 1 <= ref ? i <= ref : i >= ref; x = 1 <= ref ? ++i : --i) {
+        results.push((function() {
+          var j, ref1, results1;
+          results1 = [];
+          for (y = j = 1, ref1 = gridHeight; 1 <= ref1 ? j <= ref1 : j >= ref1; y = 1 <= ref1 ? ++j : --j) {
+            results1.push(number);
+          }
+          return results1;
+        })());
+      }
+      return results;
+    })();
+  };
+
+  randomCell = function(cells) {
+    var results, x, y;
+    results = [];
+    while (true) {
+      x = Math.floor(Math.random() * cells.length);
+      y = Math.floor(Math.random() * cells[0].length);
+      if (!cells[x][y]) {
+        cells[x][y] = 1;
+        break;
+      } else {
+        results.push(void 0);
+      }
     }
-    return new_list;
+    return results;
   };
 
   canvas = $('canvas')[0];
@@ -89,48 +123,37 @@
 
   ctx.strokeStyle = '#a3a3a3';
 
-  for (x = j = 0, ref = gridWidth - 1; 0 <= ref ? j <= ref : j >= ref; x = 0 <= ref ? ++j : --j) {
+  cellColor = '#999999';
+
+  blightColor = '#e05353';
+
+  for (x = i = 0, ref = gridWidth - 1; 0 <= ref ? i <= ref : i >= ref; x = 0 <= ref ? ++i : --i) {
     ctx.beginPath();
     ctx.moveTo(x * tileWidth, 0);
     ctx.lineTo(x * tileWidth, canvas.height);
     ctx.stroke();
   }
 
-  for (y = k = 0, ref1 = gridHeight - 1; 0 <= ref1 ? k <= ref1 : k >= ref1; y = 0 <= ref1 ? ++k : --k) {
+  for (y = j = 0, ref1 = gridHeight - 1; 0 <= ref1 ? j <= ref1 : j >= ref1; y = 0 <= ref1 ? ++j : --j) {
     ctx.beginPath();
     ctx.moveTo(0, y * tileHeight);
     ctx.lineTo(canvas.width, y * tileHeight);
     ctx.stroke();
   }
 
-  cells = (function() {
-    var l, ref2, results;
-    results = [];
-    for (i = l = 1, ref2 = gridWidth; 1 <= ref2 ? l <= ref2 : l >= ref2; i = 1 <= ref2 ? ++l : --l) {
-      results.push(new Array(gridHeight));
-    }
-    return results;
-  })();
+  cells = makeGrid(0, gridWidth, gridHeight);
 
-  cells[11][10] = true;
-
-  cells[11][11] = true;
-
-  cells[11][12] = true;
-
-  cells[11][11] = true;
-
-  cells[12][12] = true;
-
-  cells[12][13] = true;
+  randomCell(cells);
 
   drawCells(cells, tileWidth, tileHeight);
 
   setInterval(function() {
-    cells = updateCells(cells);
-    return drawCells(cells, tileWidth, tileHeight);
-  }, 500);
+    return randomCell(cells);
+  }, 1000);
 
-  setInterval(function() {});
+  setInterval(function() {
+    cells = updateCells(cells);
+    return drawCells(cells, tileWidth, tileHeight, cellColor, blightColor);
+  }, 100);
 
 }).call(this);
